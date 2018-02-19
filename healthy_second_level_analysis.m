ce63e819b5eb56fbde3fa70321b26610c7a396f5
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-02-19 11:46:51 +0000

    one sample t-tests for conditions, not differences

--- healthy_second_level_analysis.m
+++ healthy_second_level_analysis.m
@@ -26,26 +26,34 @@ end
 % set up dir structure for group-level analyses
 matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent = {[bids_dir filesep 'derivatives']};
 matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'group';
-matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = cfg_dep('Make Directory: Make Directory ''group''', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = ...
+    cfg_dep('Make Directory: Make Directory ''group''', ...
+    substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'healthy';
 
 
-%% One-sample t-test for  effect
+%% One-sample t-test for ss condition
 
-matlabbatch{3}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = cfg_dep('Make Directory: Make Directory ''healthy''', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
-matlabbatch{3}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'incongruency';
+matlabbatch{3}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = ...
+    cfg_dep('Make Directory: Make Directory ''healthy''', ...
+    substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
+matlabbatch{3}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'ss';
 
-% factorial design for one-sample t-test on delta constrast of incogruency
-% effect
-matlabbatch{4}.spm.stats.factorial_design.dir(1) = cfg_dep('Make Directory: Make Directory ''incongruency''', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+% factorial design for one-sample t-test on beta for ss
+matlabbatch{4}.spm.stats.factorial_design.dir(1) = ...
+    cfg_dep('Make Directory: Make Directory ''ss''', ...
+    substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 
-% get boosted contrast for incongruency
-boosted_inc_con =  {};
+% get boosted beta for ss
+boosted_ss_beta =  {};
 for idx = 1:size(subj, 1)
-     boosted_inc_con{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_con_0004.nii,1'];
+     boosted_ss_beta{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_beta_0001.nii,1'];
 end
 
-matlabbatch{4}.spm.stats.factorial_design.des.t1.scans = boosted_inc_con';
+matlabbatch{4}.spm.stats.factorial_design.des.t1.scans = boosted_ss_beta';
 
 matlabbatch{4}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{4}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
@@ -55,31 +63,42 @@ matlabbatch{4}.spm.stats.factorial_design.masking.em = {''};
 matlabbatch{4}.spm.stats.factorial_design.globalc.g_omit = 1;
 matlabbatch{4}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 matlabbatch{4}.spm.stats.factorial_design.globalm.glonorm = 1;
-matlabbatch{5}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{5}.spm.stats.fmri_est.spmmat(1) = ...
+    cfg_dep('Factorial design specification: SPM.mat File', ...
+    substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
 matlabbatch{5}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{5}.spm.stats.fmri_est.method.Classical = 1;
-matlabbatch{6}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{6}.spm.stats.con.consess{1}.tcon.name = 'incongruency';
+matlabbatch{6}.spm.stats.con.spmmat(1) = ...
+    cfg_dep('Model estimation: SPM.mat File', ...
+    substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
+matlabbatch{6}.spm.stats.con.consess{1}.tcon.name = 'ss';
 matlabbatch{6}.spm.stats.con.consess{1}.tcon.weights = 1;
 matlabbatch{6}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{6}.spm.stats.con.delete = 0;
 
-%% One-sample t-test for semantic effect
+%% One-sample t-test for cp effect
 
-matlabbatch{7}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = cfg_dep('Make Directory: Make Directory ''healthy''', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
-matlabbatch{7}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'semantic';
+matlabbatch{7}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = ...
+    cfg_dep('Make Directory: Make Directory ''healthy''', ...
+    substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
+matlabbatch{7}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'cp';
 
-% factorial design for one-sample t-test on delta constrast of semantic
-% effect
-matlabbatch{8}.spm.stats.factorial_design.dir(1) = cfg_dep('Make Directory: Make Directory ''semantic''', substruct('.','val', '{}',{7}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+% factorial design for one-sample t-test on beta for cs
+matlabbatch{8}.spm.stats.factorial_design.dir(1) = ...
+    cfg_dep('Make Directory: Make Directory ''cp''', ...
+    substruct('.','val', '{}',{7}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 
-% get boosted contrast for semantic
-boosted_semantic =  {};
+% get boosted beta for cp
+boosted_cp_beta =  {};
 for idx = 1:size(subj, 1)
-     boosted_semantic{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_con_0008.nii,1'];
+     boosted_cp_beta{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_beta_0005.nii,1'];
 end
 
-matlabbatch{8}.spm.stats.factorial_design.des.t1.scans = boosted_semantic';
+matlabbatch{8}.spm.stats.factorial_design.des.t1.scans = boosted_cp_beta';
 
 matlabbatch{8}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{8}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
@@ -89,32 +108,43 @@ matlabbatch{8}.spm.stats.factorial_design.masking.em = {''};
 matlabbatch{8}.spm.stats.factorial_design.globalc.g_omit = 1;
 matlabbatch{8}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 matlabbatch{8}.spm.stats.factorial_design.globalm.glonorm = 1;
-matlabbatch{9}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{8}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{9}.spm.stats.fmri_est.spmmat(1) = ...
+    cfg_dep('Factorial design specification: SPM.mat File', ...
+    substruct('.','val', '{}',{8}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
 matlabbatch{9}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{9}.spm.stats.fmri_est.method.Classical = 1;
-matlabbatch{10}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{9}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{10}.spm.stats.con.spmmat(1) = ...
+    cfg_dep('Model estimation: SPM.mat File', ...
+    substruct('.','val', '{}',{9}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
 matlabbatch{10}.spm.stats.con.consess{1}.tcon.name = 'semantic';
 matlabbatch{10}.spm.stats.con.consess{1}.tcon.weights = 1;
 matlabbatch{10}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{10}.spm.stats.con.delete = 0;
 
 
-%% One-sample t-test for phonology effect
+%% One-sample t-test for cs effect
 
-matlabbatch{11}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = cfg_dep('Make Directory: Make Directory ''healthy''', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
-matlabbatch{11}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'phonology';
+matlabbatch{11}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = ...
+    cfg_dep('Make Directory: Make Directory ''healthy''', ...
+    substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
+matlabbatch{11}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'cs';
 
-% factorial design for one-sample t-test on delta constrast of phonology
-% effect
-matlabbatch{12}.spm.stats.factorial_design.dir(1) = cfg_dep('Make Directory: Make Directory ''phonology''', substruct('.','val', '{}',{11}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+% factorial design for one-sample t-test on beta for cs
+matlabbatch{12}.spm.stats.factorial_design.dir(1) = ...
+    cfg_dep('Make Directory: Make Directory ''cs''', ...
+    substruct('.','val', '{}',{11}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 
-% get boosted contrast for phono
-boosted_phono =  {};
+% get boosted beta for cs
+boosted_cs_beta =  {};
 for idx = 1:size(subj, 1)
-     boosted_phono{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_con_0007.nii,1'];
+     boosted_cs_beta{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_beta_0009.nii,1'];
 end
 
-matlabbatch{12}.spm.stats.factorial_design.des.t1.scans = boosted_phono';
+matlabbatch{12}.spm.stats.factorial_design.des.t1.scans = boosted_cs_beta';
 
 matlabbatch{12}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{12}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
@@ -124,32 +154,43 @@ matlabbatch{12}.spm.stats.factorial_design.masking.em = {''};
 matlabbatch{12}.spm.stats.factorial_design.globalc.g_omit = 1;
 matlabbatch{12}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 matlabbatch{12}.spm.stats.factorial_design.globalm.glonorm = 1;
-matlabbatch{13}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{12}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{13}.spm.stats.fmri_est.spmmat(1) = ...
+    cfg_dep('Factorial design specification: SPM.mat File', ...
+    substruct('.','val', '{}',{12}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
 matlabbatch{13}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{13}.spm.stats.fmri_est.method.Classical = 1;
-matlabbatch{14}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{13}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{14}.spm.stats.con.consess{1}.tcon.name = 'phonology';
+matlabbatch{14}.spm.stats.con.spmmat(1) = ...
+    cfg_dep('Model estimation: SPM.mat File', ...
+    substruct('.','val', '{}',{13}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
+matlabbatch{14}.spm.stats.con.consess{1}.tcon.name = 'cs';
 matlabbatch{14}.spm.stats.con.consess{1}.tcon.weights = 1;
 matlabbatch{14}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{14}.spm.stats.con.delete = 0;
 
 
-%% One-sample t-test for unrelated effect
+%% One-sample t-test for us effect
 
-matlabbatch{15}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = cfg_dep('Make Directory: Make Directory ''healthy''', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+matlabbatch{15}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent(1) = ...
+    cfg_dep('Make Directory: Make Directory ''healthy''', ...
+    substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 matlabbatch{15}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'unrelated';
 
-% factorial design for one-sample t-test on delta constrast of unrelated
-% effect
-matlabbatch{16}.spm.stats.factorial_design.dir(1) = cfg_dep('Make Directory: Make Directory ''unrelated''', substruct('.','val', '{}',{15}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
+% factorial design for one-sample t-test on beta for us
+matlabbatch{16}.spm.stats.factorial_design.dir(1) = ...
+    cfg_dep('Make Directory: Make Directory ''us''', ...
+    substruct('.','val', '{}',{15}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','dir'));
 
-% get boosted contrast for semantic
-boosted_unrelated =  {};
+% get boosted beta for semantic
+boosted_us_beta =  {};
 for idx = 1:size(subj, 1)
-     boosted_unrelated{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_con_0009.nii,1'];
+     boosted_us_beta{idx} = [subjects{idx} filesep 'boosted_stats/hrf_boost/sboost_beta_0013.nii,1'];
 end
 
-matlabbatch{16}.spm.stats.factorial_design.des.t1.scans = boosted_semantic';
+matlabbatch{16}.spm.stats.factorial_design.des.t1.scans = boosted_us_beta';
 
 matlabbatch{16}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{16}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
@@ -159,11 +200,17 @@ matlabbatch{16}.spm.stats.factorial_design.masking.em = {''};
 matlabbatch{16}.spm.stats.factorial_design.globalc.g_omit = 1;
 matlabbatch{16}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 matlabbatch{16}.spm.stats.factorial_design.globalm.glonorm = 1;
-matlabbatch{17}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{16}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{17}.spm.stats.fmri_est.spmmat(1) = ...
+    cfg_dep('Factorial design specification: SPM.mat File', ...
+    substruct('.','val', '{}',{16}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
 matlabbatch{17}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{17}.spm.stats.fmri_est.method.Classical = 1;
-matlabbatch{18}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{17}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{18}.spm.stats.con.consess{1}.tcon.name = 'unrelated';
+matlabbatch{18}.spm.stats.con.spmmat(1) = ...\
+    cfg_dep('Model estimation: SPM.mat File', ...
+    substruct('.','val', '{}',{17}, '.','val', '{}',{1}, '.','val', '{}',{1}), ...
+    substruct('.','spmmat'));
+matlabbatch{18}.spm.stats.con.consess{1}.tcon.name = 'us';
 matlabbatch{18}.spm.stats.con.consess{1}.tcon.weights = 1;
 matlabbatch{18}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{18}.spm.stats.con.delete = 0;
