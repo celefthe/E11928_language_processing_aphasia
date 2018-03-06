5701e5020195abd351eede7d062728635d0ff095
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-03-06 22:12:17 +0000

    simplify T1 averaging

--- avg_t1.m
+++ avg_t1.m
@@ -16,18 +16,20 @@ subj = dir('sub-healthy*');
 for idx = 1:size(subj,1)
      subjects{idx}.path = [bids_dir filesep 'derivatives' filesep subj(idx).name];
      subjects{idx}.t1img = [subjects{idx}.path filesep 'anat' filesep 'normal_' subj(idx).name '_ses-01_T1w.nii'];
-     t1_images = [t1_images; subjects{idx}.t1img];
+     if idx == 1
+         t1_images = [subjects{idx}.t1img];
+     else
+         t1_images = [t1_images; subjects{idx}.t1img];
+     end
 end
 
 volume_headers = spm_vol(t1_images);
-volume_data = spm_read_vols(volume_headers(1));
-t1_average = zeros(size(volume_data));
+volume_data = spm_read_vols(volume_headers);
+t1_average = mean(volume_data,4);
+W = volume_headers(1);
+W.fname = [bids_dir filesep 'derivatives' filesep 'group' filesep 'healthy_t1avg.nii'];
+W.descrip = 'Average of 16 healthy normalized T1 controls';
+W.private.descrip = W.descrip;
+spm_write_vol(W,t1_average);
 
-for idx=1:size(t1_images,1),
-   volume_data = spm_read_vols(volume_headers(idx));
-   t1_average = t1_average + volume_data/size(t1_images,1);
-end
 
-volume_headers(1).fname = ...
-    [bids_dir filesep 'derivatives' filesep 'group' filesep 'healthy' filesep 'anat' filesep 'healthy_t1avg.nii']
-spm_write_vol(volume_headers(1),t1_average);
